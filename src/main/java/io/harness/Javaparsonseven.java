4778d9e053aaba47c15d04d7a366a6ef0cb1625d
GitHubApp <>
2025-05-21 14:17:17 +0000

    Project init

---
+++ src/main/java/io/harness/Javaparsonseven.java
@@ -0,0 +1,11 @@
+package io.harness;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Javaparsonseven {
+    public static void main(String[] args) {
+        SpringApplication.run(Javaparsonseven.class, args);
+    }
+}
